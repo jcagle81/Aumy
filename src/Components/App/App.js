0a393c82c43aaca254ee7be7c63b6847ac117d79
jcagle81 <57734109+jcagle81@users.noreply.github.com>
2021-05-05 20:43:24 -0500

    navbar

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,24 @@
+import React from 'react';
 import './App.css';
 import Header from '../Header/Header';
+import { Jumbotron, Container, Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import three from './three.jpg';
+import Navigation from '../Navbar/Navbar';
 
 function App() {
+    
   return (
     <div className="App">
       <Header />
+      <div>
+        <Jumbotron fluid className="jumbocarousel">
+          <Container fluid>
+          <img src={three} width="1000"/>
+          </Container>
+        </Jumbotron>
+        <Navigation />
+           
+      </div>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,25 @@
+import React from "react";
+import headerimg from './headerimg.png';
+import './Header.css';
+import { Jumbotron, Container } from 'reactstrap';
+
 function Header() {
     return (
-        <p>Thành công của bạn là mục tiêu của chúng tôi!</p>
+        <React.Fragment>
+            <Jumbotron fluid className="jumboheader">
+                <Container fluid>
+                <h1 className="display-3">Thành công của bạn là mục tiêu của chúng tôi!</h1>
+                <p className="lead"></p>
+                </Container>
+            </Jumbotron>
+    
+            <img src={headerimg} />
+        
+                                           
+        </React.Fragment>
+        
+            
+        
     );
 }
 
